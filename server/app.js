75310ae7a6ced3207d9dffca2127f357a6e38bb4
18Kailash <sumer.ki359@gmail.com>
2023-08-14 13:29:58 +0530

    chat-app

--- server/app.js
+++ server/app.js
@@ -12,13 +12,13 @@ const io = new Server(server, {
 });
 
 const _dirname = path.dirname("");
-const buildPath = path.join(_dirname, "../client/build");
+const buildPath = path.join(_dirname, "../frontend/build");
 
 app.use(express.static(buildPath));
 
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "../client/build/index.html"),
+    path.join(__dirname, "../frontend/build/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
